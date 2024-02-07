60e9a730d376da2596e83c8ae42b684f006e582d
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-07 15:19:16 +0100

    Add more dimensions to histogram query

--- histogram.sql
+++ histogram.sql
@@ -1,10 +1,103 @@
--- Very few measurements in 70-79 range, does not follow the pattern
+-- YES: Very few measurements in 70 -79 range, does not follow the pattern
 WITH ranges AS (
-    SELECT (ten*10)::text||'-'||(ten*10+9)::text AS range,
-           ten*10 AS "min", ten*10+9 AS "max", ten as id
-      FROM generate_series(0,22) AS t(ten))
-SELECT r.range, count(speeds.*)
-  FROM speeds
-  RIGHT JOIN ranges r ON speed BETWEEN r.min AND r.max
- GROUP BY r.range, r.id
- ORDER BY r.id;
+    SELECT
+        (ten * 10) :: text || '-' ||(ten * 10 + 9) :: text AS "range",
+        ten * 10 AS "min",
+        ten * 10 + 9 AS "max",
+        ten as id
+    FROM
+        generate_series(0, 22) AS t(ten)
+)
+SELECT
+    r.range,
+    count(speeds.*)
+FROM
+    speeds
+    RIGHT JOIN ranges r ON speed BETWEEN r."min"
+    AND r."max"
+GROUP BY
+    r."range",
+    r.id
+ORDER BY
+    r.id;
+    
+-- NO: diff per year
+WITH ranges AS (
+    SELECT
+        (ten * 10) :: text || '-' ||(ten * 10 + 9) :: text AS "range",
+        ten * 10 AS "min",
+        ten * 10 + 9 AS "max",
+        ten as id
+    FROM
+        generate_series(0, 22) AS t(ten)
+)
+SELECT
+    r.range,
+    count(speeds.*),
+    "year"
+FROM
+    speeds
+    RIGHT JOIN ranges r ON speed BETWEEN r."min"
+    AND r."max"
+    JOIN timetbl USING (timid)
+GROUP BY
+    r.id,
+    r."range",
+    "year"
+ORDER BY
+    "year",
+    r.id;
+    
+-- NO: diff per category
+WITH ranges AS (
+    SELECT
+        (ten * 10) :: text || '-' ||(ten * 10 + 9) :: text AS "range",
+        ten * 10 AS "min",
+        ten * 10 + 9 AS "max",
+        ten as id
+    FROM
+        generate_series(0, 22) AS t(ten)
+)
+SELECT
+    r.range,
+    count(speeds.*),
+    category
+FROM
+    speeds
+    RIGHT JOIN ranges r ON speed BETWEEN r."min"
+    AND r."max"
+    JOIN car USING (carid)
+GROUP BY
+    r.id,
+    r."range",
+    category
+ORDER BY
+    category,
+    r.id;
+
+-- MAYBE: Only Netherlands have high counts in ['30-39', '40-49', '50-59', '60-69'] ranges, but not in '70-79', which could explain why the total count is very low
+WITH ranges AS (
+    SELECT
+        (ten * 10) :: text || '-' ||(ten * 10 + 9) :: text AS "range",
+        ten * 10 AS "min",
+        ten * 10 + 9 AS "max",
+        ten as id
+    FROM
+        generate_series(0, 22) AS t(ten)
+)
+SELECT
+    r.range,
+    count(speeds.*),
+    country
+FROM
+    speeds
+    RIGHT JOIN ranges r ON speed BETWEEN r."min"
+    AND r."max"
+    JOIN "location" USING (locid)
+GROUP BY
+    r.id,
+    r."range",
+    country
+ORDER BY
+    country,
+    r.id;
