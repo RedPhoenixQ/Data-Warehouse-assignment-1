c136450a45c44455d004b739a8fca831871e7516
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-15 13:50:22 +0100

    Update histogram query to remove unused 'id' field

--- patterns/Netherlands.sql
+++ patterns/Netherlands.sql
@@ -34,28 +34,23 @@ GROUP BY
 -- ORDER BY
 --     "year";
 -- Netherlands histogram
+
 WITH ranges AS (
-    SELECT
-        lpad((ten * 10) :: text, 3, '0') || '-' || lpad((ten * 10 + 9) :: text, 3, '0') AS "range",
-        ten * 10 AS "min",
-        ten * 10 + 9 AS "max",
-        ten as id
-    FROM
-        generate_series(0, 22) AS t(ten)
-)
+	SELECT
+		lpad((ten * 10)::text, 3, '0') || '-' || lpad((ten * 10 + 9)::text, 3, '0') AS "range",
+		ten * 10 AS "min",
+		ten * 10 + 9 AS "max",
+	FROM
+		generate_series(0, 22) AS t (ten))
 SELECT
-    r.range,
-    count(speeds.*),
-    country
+	r.range,
+	count(speeds.*),
+	country
 FROM
-    speeds
-    RIGHT JOIN ranges r ON speed BETWEEN r."min"
-    AND r."max"
-    JOIN "location" USING (locid)
+	speeds
+	RIGHT JOIN ranges r ON speed BETWEEN r. "min" AND r. "max"
+	JOIN "location" USING (locid)
 GROUP BY
-    r.id,
-    r."range",
-    country
+	r. "range", country
 ORDER BY
-    country,
-    r.id;
+	country, r. "range";
